8d7ea4f61dbf42059516752ba7174137812d99f9
Adarasha Gaihre <adarashagaihre80@nepathyacollege.edu.np>
2026-09-14 00:10:36 +0545

    Add: duplicate-integer - submission-6

---
+++ Data Structures & Algorithms/duplicate-integer/submission-6.go
@@ -0,0 +1,11 @@
+func hasDuplicate(nums []int) bool {
+    // Thsi time i will try to use map
+    seen := make(map[int]bool);
+    for _,v := range nums{
+        if seen[v]{
+            return true;
+        }
+        seen[v]=true;
+    }
+    return false
+}
